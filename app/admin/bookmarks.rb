7a710a34d5f1898791e0b9bcbd964401489986b4
Raghu Betina <raghu@firstdraft.com>
2018-04-02 23:13:07 +0000

    Whitelist columns in Bookmark for ActiveAdmin

--- app/admin/bookmarks.rb
+++ app/admin/bookmarks.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Bookmark do
+
+ permit_params :movie_id, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
