af8ef73f6937ccb53ec09597bcf85490f7e0cc5d
Raghu Betina <raghu@firstdraft.com>
2018-04-02 23:12:09 +0000

    Validate presence of castings.actor_id

--- app/models/casting.rb
+++ app/models/casting.rb
@@ -5,6 +5,8 @@ class Casting < ApplicationRecord
 
   # Validations
 
+  validates :actor_id, :presence => true
+
   validates :character, :presence => true
 
   validates :movie_id, :presence => true
