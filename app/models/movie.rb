d4a7d855cc939384e0b285a5f047ab26c8ce6a92
Raghu Betina <raghu@firstdraft.com>
2018-04-02 23:12:09 +0000

    Indirect association between movies and actors

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,6 +11,10 @@ class Movie < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :actors,
+             :through => :castings,
+             :source => :actor
+
   # Validations
 
   validates :title, :presence => true
